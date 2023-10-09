645d04953c16c6315e959c02bc5d73c6be08ff24
Mohit <mohitkashyap46707@gmail.com>
2023-10-09 23:11:55 +0530

    The YouTube api is dobe

--- app.py
+++ app.py
@@ -8,30 +8,44 @@ def index():
 
 @app.get("/yt")
 def yt():
+  # required modules ko import kiya ja raha hai
   from pytube import YouTube
   from urllib.parse import unquote
+
   url = request.args.get("url")
-  if not url:
-    return "URL is required!"
-  else:
-    url = unquote(url)
+
+  if not url: return "URL is required!"
+  
+  url = unquote(url)
+
   try:
     yt = YouTube(url)
     streams = yt.streams
-    video_streams = streams.filter(progressive=False, only_video=True)
-    # audio_streams = streams.filter(progressive=False, only_audio=True)
-    methods = [ method for method in dir(video_streams) if not method.startswith("_") ]
-    json_data = {}
-    for method in methods:
-      prop = getattr(video_streams, method)
-      if not callable(prop):
-        json_data[method] = prop
-    return jsonify(json_data)
+    audio_streams = []
+    video_streams = []
+
+    methods = [ 'abr', 'audio_codec', 'bitrate', 'codecs', 'default_filename', 'expiration', 'filesize', 'filesize_approx', 'filesize_gb', 'filesize_kb', 'filesize_mb', 'includes_audio_track', 'includes_video_track', 'is_3d', 'is_adaptive', 'is_dash', 'is_hdr', 'is_live', 'is_otf', 'is_progressive', 'itag', 'mime_type', 'resolution', 'subtype', 'title', 'type', 'url', 'video_codec' ]
+
+    for stream in streams.filter(only_audio=True):
+      audio_data = { method: getattr(stream, method) for method in methods }
+      audio_streams.append(audio_data)
+
+    methods.insert(11, "fps")
+
+    for stream in streams.filter(only_video=True):
+      video_data = { method: getattr(stream, method) for method in methods }
+      video_streams.append(video_data)
+
+    return jsonify({ "videos": video_streams, "audios": audio_streams })
   except Exception as e:
-    return "Something went wrong!"
+    print(e)
+    return str(e)
 
 if __name__ == "__main__":
+  """
   import sys
   from gunicorn.app.wsgiapp import run
   sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
   sys.exit(run())
+  """
+  app.run(debug=True)
