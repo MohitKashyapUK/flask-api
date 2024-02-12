dd347165d3ff5e0ac26953346962503e79639fa7
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2024-02-12 21:46:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,38 +8,42 @@ def index():
 
 @app.get("/yt")
 def yt():
-  # required modules ko import kiya ja raha hai
-  from pytube import YouTube
+  from pytube import YouTube, request
   from urllib.parse import unquote
-
-  url = request.args.get("url") # parameter
-
-  if not url: return "URL is required!"
   
-  url = unquote(url)
+  URL = unquote(request.args.get("url"))
 
-  try:
-    yt = YouTube(url)
-    streams = yt.streams
-    audio_streams = []
-    video_streams = []
-
-    methods = [ 'abr', 'audio_codec', 'bitrate', 'codecs', 'default_filename', 'expiration', 'filesize', 'filesize_approx', 'filesize_gb', 'filesize_kb', 'filesize_mb', 'includes_audio_track', 'includes_video_track', 'is_3d', 'is_adaptive', 'is_dash', 'is_hdr', 'is_live', 'is_otf', 'is_progressive', 'itag', 'mime_type', 'resolution', 'subtype', 'title', 'type', 'url', 'video_codec' ]
-
-    for stream in streams.filter(only_audio=True):
-      audio_data = { method: getattr(stream, method) for method in methods }
-      audio_streams.append(audio_data)
-
-    methods.insert(11, "fps")
-
-    for stream in streams.filter(only_video=True):
-      video_data = { method: getattr(stream, method) for method in methods }
-      video_streams.append(video_data)
-
-    return jsonify({ "videos": video_streams, "audios": audio_streams })
-  except Exception as e:
-    print(e)
-    return str(e)
+  # Initializing downloader
+  yt = YouTube(URL)
+  streams = yt.streams.asc()
+  
+  obj = { "with_audio": [], "without_audio": [], "only_audio": [], "video_info": {} }
+  
+  first_stream = streams[0]
+  obj["video_info"]["title"] = first_stream.title
+  
+  for stream_obj in streams:
+    stream_type = stream_obj.type
+    custom_streams = { "type": stream_type, "subtype": stream_obj.subtype, "url": stream_obj.url }
+    # calculating the size of the video
+    filesize_kb = stream_obj.filesize_kb
+    if filesize_kb > 1000:
+      filesize_mb = stream_obj.filesize_mb
+      if filesize_mb > 1000:
+        custom_streams["size"] = str(stream_obj.filesize_gb) + "GB"
+      else:
+        custom_streams["size"] = str(filesize_mb) + "MB"
+    else:
+      custom_streams["size"] = str(filesize_kb) + "KB"
+    # categorizing the videos
+    if stream_type == "video":
+      custom_streams["fps"] = stream_obj.fps
+      custom_streams["resolution"] = stream_obj.resolution
+    if stream_type == "video":
+      obj["with_audio" if stream_obj.is_progressive == True else "without_audio"].append(custom_streams)
+    else:
+      obj["only_audio"].append(custom_streams)
+  return obj
 
 if __name__ == "__main__":
   import sys
