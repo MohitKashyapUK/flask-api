38cde56dcf33616893e8f6e9449c2a14f1887ea6
Mohit Kashyap <92683467+MohitKashyapUK@users.noreply.github.com>
2024-02-24 18:04:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Home page
 @app.get("/")
 def index():
   return "<h1>Hello, this is a home page!</h1>"
 
+# Ye code YouTube kee videos kee information deta hai
 @app.get("/yt")
 def yt():
   from pytube import YouTube
@@ -13,7 +15,7 @@ def yt():
   
   URL = unquote(request.args.get("url"))
 
-  # Initializing downloader
+  # Initializing the downloader
   yt = YouTube(URL)
   streams = yt.streams.asc()
   
